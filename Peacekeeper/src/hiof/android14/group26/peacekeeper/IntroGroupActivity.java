d23729e81c9831d22a97ea82769a5562bbd0d0b4
RobinKarlsen <robikar91@gmail.com>
2014-04-28 17:33:03 +0200

    Minor changes, mainly involving strings and linking those up to buttons. including the info button on the intro group layout.

--- Peacekeeper/src/hiof/android14/group26/peacekeeper/IntroGroupActivity.java
+++ Peacekeeper/src/hiof/android14/group26/peacekeeper/IntroGroupActivity.java
@@ -3,6 +3,8 @@ package hiof.android14.group26.peacekeeper;
 import android.os.Bundle;
 import android.view.View;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 public class IntroGroupActivity extends Activity {
@@ -12,6 +14,7 @@ public class IntroGroupActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.intro_group);
+			
 	}
 	
 	public void startCreateGroupActivity(View view) {
@@ -25,4 +28,29 @@ public class IntroGroupActivity extends Activity {
 		
 		startActivity(intent);
 	}
+	
+	public void showInfo(View view){
+		// prepare the alert box                  
+	    AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+	    
+	    // Strings used
+	    String okBtn = getResources().getString(R.string.ok);
+	    String alertBoxContent = getResources().getString(R.string.infoBox);
+	     
+	    // set the message to display
+	    alertbox.setMessage(alertBoxContent);
+	     
+	    // add a neutral button to the alert box and assign a click listener
+	    alertbox.setNeutralButton(okBtn, new DialogInterface.OnClickListener() {
+	         
+	        // click listener on the alert box
+	        public void onClick(DialogInterface arg0, int arg1) {
+	            // the button was clicked
+	        }
+	    });
+	     
+	    // show it
+	    alertbox.show();
+	}
+	
 }
